556c4859c22afffc57737864217eeff53da80497
Adarsh <adarsh9472351@gmail.com>
2022-05-25 10:41:07 +0530

    payment-gateway

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongodb = require("mongodb");
 const { MongoClient } = mongodb;
-const mongoURI =
-  "mongodb+srv://admin-adarsh:adarsh123@cluster0.jshdb.mongodb.net/TripConnect?retryWrites=true&w=majority";
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.jshdb.mongodb.net/${process.env.MONGODB_DATABASENAME}?retryWrites=true&w=majority`;
 
 async function connectedDatabase() {
   const client = await MongoClient.connect(mongoURI);
